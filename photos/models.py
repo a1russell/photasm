833c75e95c81c7389fa825980a82d33c4c44e43d
Adam Russell <a6russell@gmail.com>
2009-11-08 03:15:40 -0500

    Added some initial models.

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Album(models.Model):
+    owner = models.ForeignKey(User)
+    name = models.CharField(max_length=64)
+
+    def __unicode__(self):
+        return self.name
+
+
+class PhotoTag(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Photo(models.Model):
+    owner = models.ForeignKey(User)
+
+    # Exif ImageDescription, IPTC Caption 
+    _description = models.CharField(max_length=2000)
+
+    # Exif Artist, IPTC Byline
+    _artist = models.CharField(max_length=32)
+
+    # IPTC CountryName
+    _country = models.CharField(max_length=64)
+
+    # IPTC ProvinceState
+    _state = models.CharField(max_length=32)
+
+    # IPTC City
+    _city = models.CharField(max_length=32)
+
+    # IPTC SubLocation
+    _location = models.CharField(max_length=32)
+
+    # Exif DateTimeOriginal, IPTC DateCreated
+    _date_created = models.DateField()
+
+    # IPTC Keywords
+    _keywords = models.ManyToManyField(PhotoTag)
+
+    # Exif ImageWidth
+    _image_width = models.IntegerField()
+
+    # Exif ImageHeight
+    _image_height = models.IntegerField()
+
+    album = models.ForeignKey(Album)
+
+    def __unicode__(self):
+        return self.description
+
