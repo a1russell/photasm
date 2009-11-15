7d914f4d90eda44df7122813755302733f12dd0d
adam <none@none>
2009-11-14 19:51:01 -0500

    Configured URL for uploaded media.

--- urls.py
+++ urls.py
@@ -4,7 +4,9 @@ from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
+    
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
@@ -15,7 +17,17 @@ urlpatterns = patterns('',
 )
 
 if settings.DEBUG:
-    urlpatterns += patterns('',
+    # Serve static media
+    urlpatterns += patterns(
+        '',
         (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
          {'document_root': settings.STATIC_DOC_ROOT}),
+    )
+    
+    # Serve uploaded media
+    # The URL regex for this pattern should be related to settings.MEDIA_URL.
+    urlpatterns += patterns(
+        '',
+        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT}),
     )
