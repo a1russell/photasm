74112f08cbbb412523a172c420cf6158cb7ee8c8
adam <none@none>
2009-11-15 14:05:23 -0500

    Moved templates to root of project.
    --HG--
    rename : photos/templates/photo_detail.html => templates/photos/photo_detail.html
    rename : photos/templates/photo_list.html => templates/photos/photo_list.html

--- photos/urls.py
+++ photos/urls.py
@@ -13,8 +13,10 @@ urlpatterns += patterns(
     'django.views.generic',
     
     url(r'^$', 'list_detail.object_list',
-        dict(info_dict, template_name="photo_list.html"), "photo_list"),
+        dict(info_dict, template_name="photos/photo_list.html"),
+        "photo_list"),
     
     url(r'^(?P<object_id>\d+)/$', 'list_detail.object_detail',
-        dict(info_dict, template_name="photo_detail.html"), "photo_detail"),
+        dict(info_dict, template_name="photos/photo_detail.html"),
+        "photo_detail"),
 )
