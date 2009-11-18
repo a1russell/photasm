6f463510bd1ebc3a9f1f5b1aa0d7d95d0bd5b04b
adam <none@none>
2009-11-18 09:35:26 -0500

    Added positional argument pass-through to photos_list and photo_detail views.

--- photos/views.py
+++ photos/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.views.generic import list_detail
 
-def photo_list(request, queryset, **kwargs):
-    return list_detail.object_list(request, queryset, **kwargs)
+def photo_list(request, queryset, *args, **kwargs):
+    return list_detail.object_list(request, queryset, *args, **kwargs)
 
-def photo_detail(request, queryset, **kwargs):
-    return list_detail.object_detail(request, queryset, **kwargs)
+def photo_detail(request, queryset, *args, **kwargs):
+    return list_detail.object_detail(request, queryset, *args, **kwargs)
