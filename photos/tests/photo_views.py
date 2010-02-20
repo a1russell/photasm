c5c0051216e23a4357ad4da994c8b80b926d0442
Adam Russell <adamlr6+pub@gmail.com>
2010-02-20 00:39:25 -0500

    Added tests for dealing with Photo objects in the Admin site.

--- photos/tests/photo_views.py
+++ photos/tests/photo_views.py
@@ -20,6 +20,9 @@ class AddPhotoTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user('adam', 'adam@example.com',
                                              'adampassword')
+        self.user.is_staff = True
+        self.user.is_superuser = True
+        self.user.save()
         self.client.login(username='adam', password='adampassword')
         self.album = Album.objects.create(owner=self.user, name='Test')
 
@@ -30,6 +33,7 @@ class AddPhotoTest(TestCase):
         metadata = pyexiv2.Image(self.image_path)
         metadata.readMetadata()
         metadata['Exif.Image.ImageDescription'] = 'test image'
+        metadata['Exif.Image.Artist'] = 'Adam'
         metadata.writeMetadata()
 
     def tearDown(self):
@@ -65,6 +69,37 @@ class AddPhotoTest(TestCase):
         # Get page back with error message
         self.assertEqual(response.status_code, 200)
 
+    def test_admin_form(self):
+        photo_upload_url = '/admin/photos/photo/add/'
+
+        image = open(self.image_path)
+        response = self.client.post(photo_upload_url, {
+            'owner': self.user.id,
+            'album': self.album.id,
+            'image': image,
+            'description': 'image for testing',
+            'metadata_sync_enabled': True,
+        })
+        image.close()
+        photos = Photo.objects.order_by('-id')
+        self.assertTrue(len(photos))
+        photo = photos[0]
+        self.assertEqual(response.status_code, 302)
+
+        self.assertFalse(photo.sync_metadata_from_file())
+        self.assertEqual(photo.description, 'image for testing')
+        self.assertEqual(photo.artist, 'Adam')
+        self.assertEqual(photo.is_jpeg, self.is_jpeg)
+        self.assertEqual(photo.owner, self.user)
+        self.assertEqual(Image.open(photo.thumbnail.path).format,
+                         self.file_format)
+        metadata = pyexiv2.Image(photo.image.path)
+        metadata.readMetadata()
+        self.assertEqual(metadata['Exif.Image.ImageDescription'],
+                         'image for testing')
+        self.assertEqual(metadata['Exif.Image.Artist'],
+                         'Adam')
+
 
 class AddTIFFTest(AddPhotoTest):
     file_format = 'TIFF'
@@ -75,10 +110,13 @@ class AddTIFFTest(AddPhotoTest):
 class EditPhotoTest(TestCase):
 
     def setUp(self):
-        user = User.objects.create_user('adam', 'adam@example.com',
-                                        'adampassword')
+        self.user = User.objects.create_user('adam', 'adam@example.com',
+                                             'adampassword')
+        self.user.is_staff = True
+        self.user.is_superuser = True
+        self.user.save()
         self.client.login(username='adam', password='adampassword')
-        self.album = Album.objects.create(owner=user, name='Test')
+        self.album = Album.objects.create(owner=self.user, name='Test')
         self.keyword = PhotoTag.objects.create(name='test')
 
         image_fd, image_path = tempfile.mkstemp(suffix='.jpg')
@@ -86,7 +124,7 @@ class EditPhotoTest(TestCase):
         Image.new('RGB', (640, 480)).save(image_path, 'JPEG')
 
         self.photo = Photo()
-        self.photo.owner = user
+        self.photo.owner = self.user
         self.photo.album = self.album
         image = open(image_path)
         self.photo.image = ImageFile(image)
@@ -128,13 +166,33 @@ class EditPhotoTest(TestCase):
         self.photo = Photo.objects.get(id=self.photo.id)
         self.assertRedirects(response, self.photo.get_absolute_url())
         self.assertFalse(self.photo.sync_metadata_to_file())
-        metadata = pyexiv2.Image(self.photo.image.path)
-        metadata.readMetadata()
-        self.assertEqual(metadata['Exif.Image.ImageDescription'],
-                         'test image')
+        self.assertEqual(self.photo.description, 'test image')
 
         response = self.client.post(photo_edit_url, {})
         self.assertEqual(response.status_code, 200)
+
+    def test_admin_form(self):
+        photo_edit_url = '/admin/photos/photo/%d/' % self.photo.id
+
+        post_data = {
+            'owner': self.user.id,
+            'album': self.album.id,
+            'description': 'test image',
+            'artist': 'Adam',
+            'country': 'USA',
+            'province_state': 'VA',
+            'city': 'Blacksburg',
+            'location': 'Drillfield',
+            'time_created': '2007-09-28 03:00:00',
+            'keywords': self.keyword.id,
+            'metadata_sync_enabled': True,
+            '_save': 'Save',
+        }
+        response = self.client.post(photo_edit_url, post_data)
+        self.photo = Photo.objects.get(id=self.photo.id)
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(self.photo.sync_metadata_to_file())
+        self.assertEqual(self.photo.description, 'test image')
 
 
 class PhotoDetailTest(TestCase):
@@ -167,6 +225,7 @@ class PhotoDetailTest(TestCase):
         os.remove(image_path)
         self.photo.set_keywords(['test', 'photo'])
         self.photo.save()
+        # TODO: Remove these two lines.
         self.photo.sync_metadata_to_file()
         self.photo.create_thumbnail()
 
