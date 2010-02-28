50a695ab8d8ca1e11471e464e6d5afc386d7dc13
Adam Russell <adamlr6+pub@gmail.com>
2010-02-28 10:42:53 -0500

    In fabfile: removed hard-coding of project name, added project_root initialization, checked that apache2 is running before restarting it.

--- fabfile.py
+++ fabfile.py
@@ -9,6 +9,10 @@ from fabric.api import (
 )
 
 
+env.project_name = 'photasm'
+env.project_root = '/usr/local/webapps/'
+
+
 def deploy():
     """\
     Deploy the latest version of the site to the servers.
@@ -18,35 +22,38 @@ def deploy():
     # Command line, per task: fab deploy:hosts="host1;host2"
     # Command line, globally: fab --hosts host1,host2 deploy
     require('hosts')
-    # Place directory in ~/.fabricrc, e.g.:
-    # project_root = /usr/local/webapps/
-    require('project_root')
+    require('project_root')  # e.g. /usr/local/webapps/
+    require('project_name')
     import time
     env.release = time.strftime('%Y%m%d%H%M%S')
-    local('hg archive --type tgz /tmp/photasm-%(release)s.tar.gz' % env)
-    put('/tmp/photasm-%(release)s.tar.gz' % env,
-        '/tmp/photasm-%(release)s.tar.gz' % env)
+    local('hg archive --type tgz '
+          '/tmp/%(project_name)s-%(release)s.tar.gz' % env)
+    put('/tmp/%(project_name)s-%(release)s.tar.gz' % env,
+        '/tmp/%(project_name)s-%(release)s.tar.gz' % env)
     with cd(env.project_root):
-        sudo('tar -xzf /tmp/photasm-%(release)s.tar.gz' % env)
-    sudo('if [[ -d %(project_root)s/photasm ]]; then '
-         'mv %(project_root)s/photasm '
-         '%(project_root)s/photasm-%(release)s.bak;'
+        sudo('tar -xzf /tmp/%(project_name)s-%(release)s.tar.gz' % env)
+    sudo('if [[ -d %(project_root)s/%(project_name)s ]]; then '
+         'mv %(project_root)s/%(project_name)s '
+         '%(project_root)s/%(project_name)s-%(release)s.bak;'
          'fi' % env)
-    sudo('mv %(project_root)s/photasm-%(release)s '
-         '%(project_root)s/photasm' % env)
-    sudo('if [[ -f %(project_root)s/photasm-%(release)s.bak/'
+    sudo('mv %(project_root)s/%(project_name)s-%(release)s '
+         '%(project_root)s/%(project_name)s' % env)
+    sudo('if [[ -f %(project_root)s/%(project_name)s-%(release)s.bak/'
          'settings_local.py ]]; then '
-         'mv %(project_root)s/photasm-%(release)s.bak/settings_local.py '
-         '%(project_root)s/photasm/;'
+         'mv %(project_root)s/%(project_name)s-%(release)s.bak/'
+         'settings_local.py '
+         '%(project_root)s/%(project_name)s/;'
          'else '
-         'cp %(project_root)s/photasm/settings_local.py.template '
-         '%(project_root)s/photasm/settings_local.py; '
+         'cp %(project_root)s/%(project_name)s/settings_local.py.template '
+         '%(project_root)s/%(project_name)s/settings_local.py; '
          'fi' % env)
-    sudo('if [[ -d %(project_root)s/photasm-%(release)s.bak ]]; then '
-         'rm -rf %(project_root)s/photasm-%(release)s.bak;'
+    sudo('if [[ -d %(project_root)s/%(project_name)s-%(release)s.bak ]]; '
+         'then '
+         'rm -rf %(project_root)s/%(project_name)s-%(release)s.bak;'
          'fi' % env)
-    sudo('chown -R root:www-admin %(project_root)s/photasm' % env)
-    sudo('chmod -R g+w %(project_root)s/photasm' % env)
-    run('rm /tmp/photasm-%(release)s.tar.gz' % env)
-    local('rm /tmp/photasm-%(release)s.tar.gz' % env)
-    sudo('apache2ctl -k graceful')
+    sudo('chown -R root:www-admin %(project_root)s/%(project_name)s' % env)
+    sudo('chmod -R g+w %(project_root)s/%(project_name)s' % env)
+    run('rm /tmp/%(project_name)s-%(release)s.tar.gz' % env)
+    local('rm /tmp/%(project_name)s-%(release)s.tar.gz' % env)
+    sudo('if ( ps -ef | grep apache2 | grep -v grep ); then '
+         'apache2ctl -k graceful; fi')
